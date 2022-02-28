1fde25e97b14ab8d82122ede917cec875723bb4e
SiaXiao <xiaosia013@gmail.com>
2022-02-28 16:47:05 -0600

    wrap up

--- mod5_solution/assignment5-solution-starter/js/script.js
+++ mod5_solution/assignment5-solution-starter/js/script.js
@@ -63,33 +63,14 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-
-// TODO: STEP 0: Look over the code from
-// *** start ***
-// to
-// *** finish ***
-// below.
-// We changed this code to retrieve all categories from the server instead of
-// simply requesting home HTML snippet. We now also have another function
-// called buildAndShowHomeHTML that will receive all the categories from the server
-// and process them: choose random category, retrieve home HTML snippet, insert that
-// random category into the home HTML snippet, and then insert that snippet into our
-// main page (index.html).
-//
-// TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
-// so it can be called when server responds with the categories data.
-
-// *** start ***
-// On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function (responseText) {
     buildAndShowHomeHTML(responseText);
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  },
+  true); 
 });
-// *** finish **
 
 
 // Builds HTML for the home page based on categories array
@@ -100,36 +81,13 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-
-      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-      // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-
-
-      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
-      // Look through this code for an example of how to do use the insertProperty function.
-      // WARNING! You are inserting something that will have to result in a valid Javascript
-      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-      // to look like. For example, a valid call would look something like this:
-      // $dc.loadMenuItems('L')
-      // Hint: you need to surround the chosen category short name with something before inserting
-      // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
-      console.log(homeHtmlToInsertIntoMainPage);
-
-
-      // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
-      // Use the existing insertHtml function for that purpose. Look through this code for an example
-      // of how to do that.
-      // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+    false); 
 }
 
 
@@ -155,8 +113,21 @@ dc.loadMenuCategories = function () {
 dc.loadAboutRankings = function() {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  buildAndShowCategoriesHTML);
+    aboutHtml,
+    function (aboutHtml) {
+      var aboutPage = buildAndShowAboutHTML(aboutHtml);
+    },
+    false);
+};
+
+function buildAndShowAboutHTML() {
+  $ajaxUtils.sendGetRequest(
+    aboutHtml,
+    function (aboutHtml) {
+      var aboutPage = buildAboutPageViewHtml(aboutHtml);
+      insertHtml("#main-content", aboutPage);
+    },
+    false);
 }
 
 
@@ -195,7 +166,6 @@ function buildAndShowCategoriesHTML (categories) {
     false);
 }
 
-
 // Using categories data and snippets html
 // build categories view HTML to be inserted into page
 function buildCategoriesViewHtml(categories,
@@ -221,6 +191,31 @@ function buildCategoriesViewHtml(categories,
   }
 
   finalHtml += "</section>";
+  return finalHtml;
+}
+
+
+function buildAboutPageViewHtml(aboutHtml) {
+
+  var finalHtml = aboutHtml;
+  finalHtml += "<br/>"
+  finalHtml += "<body>";
+
+  var ranking = Math.floor((Math.random() * 5) + 1);
+  finalHtml += "<span>" + ranking.toString() + "-star rating: </span>"
+
+   for (let i = 1; i < 6; i++) {
+    finalHtml += "<span class='{{starClass}}'></span>"
+     if (i <= ranking) {
+      finalHtml = insertProperty(finalHtml, "starClass", "fa fa-star");
+     }
+     else {
+      finalHtml = insertProperty(finalHtml, "starClass", "fa fa-star-o");
+     }
+  }
+
+  finalHtml += "</body>";
+  console.log(finalHtml);
   return finalHtml;
 }
 

4cf83321ffe281f714339a3d8fab343927ed1bb6
SiaXiao <xiaosia013@gmail.com>
2022-04-12 18:23:40 -0500

    wrap up

--- mod8_solution/app.js
+++ mod8_solution/app.js
@@ -4,25 +4,60 @@
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
-  .constant('RESTApi', 'https://davids-restaurant.herokuapp.com/menu_items.json');
+  .constant('RESTApi', 'https://davids-restaurant.herokuapp.com/menu_items.json')
+  .directive('foundItems', FoundItemsDirective)
+  .directive('listItemDescription', ListItemDescriptionDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        items: '<',
+        myTitle: '@title',
+        onRemove: '&'
+      },
+      controller: NarrowItDownController,
+      controllerAs: 'list',
+      bindToController: true
+    };
+
+    return ddo;
+  }
+
+  function ListItemDescriptionDirective() {
+    var ddo = {
+      template: '{{ item.name }} : {{ item.description }}'
+    };
+
+    return ddo;
+
+  }
+
+  function FoundItemsDirectiveController() {
+    var list = this;
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
-    var menu = this;
-    menu.searchTerm = '';
+    var list = this;
+    list.searchTerm = '';
+    list.foundItems = [];
 
-    menu.narrowDown = function() {
-      var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    list.narrowDown = function() {
+      var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
       promise.then(function(response){
-        menu.results = response;
-        console.log("menu results: ", menu.results);
-        return menu.results;
+        list.foundItems = response;
+        console.log("list results: ", list.foundItems);
       })
       .catch(function(err) {
         console.log("something went wrong", err);
       })
     }
 
+    list.removeItem = function (itemIndex) {
+      console.log("'this' is: ", this);
+      list.foundItems.splice(itemIndex, 1);
+    };
   }
 
   MenuSearchService.$inject = ['$http', 'RESTApi'];
